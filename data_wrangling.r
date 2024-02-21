312f78c3b84f36e0364da04ed234a275797c2c53
sophiasmolinski <ssmolin@uw.edu>
2024-02-20 17:01:29 -0800

    Cleaned df of unnecessary columns

--- data_wrangling.r
+++ data_wrangling.r
@@ -12,3 +12,8 @@ combined_income_college <- left_join(income_mich_df, college_df, by=c("Zip_Code"
 df_clean <- na.omit(combined_income_college)
 
 
+df_clean <- select(-id)
+
+columns_to_keep <- c("County", "City", "Zip_Code", "Area_Code", "Mean", "Median", "Stdev", "sum_w", "MathNumAssessed", "MathNumReady", "AllSbjtNumReady", "AllSbjtNumAssessed", "EBRWNumAssessed", "EBRWNumReady", "FinalMathAveScore", "FinalAllSbjtAveScore", "FinalEWBRWAveScore", "MathPctReady", "AllSbjtPctReady", "EBRWPctReady")
+
+combined_df <- select(df_clean, columns_to_keep)
