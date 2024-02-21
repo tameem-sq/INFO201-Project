f2992072bb58862cf6dce1719cc2953ffb9758c6
sophiasmolinski <ssmolin@uw.edu>
2024-02-20 18:10:20 -0800

    Made a new column called difference_of_median

--- data_wrangling.r
+++ data_wrangling.r
@@ -11,13 +11,18 @@ combined_income_college <- left_join(income_mich_df, college_df, by=c("Zip_Code"
 
 df_clean <- na.omit(combined_income_college)
 
-
-df_clean <- select(-id)
-
 columns_to_keep <- c("County", "City", "Zip_Code", "Area_Code", "Mean", "Median", "Stdev", "sum_w", "MathNumAssessed", "MathNumReady", "AllSbjtNumReady", "AllSbjtNumAssessed", "EBRWNumAssessed", "EBRWNumReady", "FinalMathAveScore", "FinalAllSbjtAveScore", "FinalEWBRWAveScore", "MathPctReady", "AllSbjtPctReady", "EBRWPctReady")
 
 combined_df <- select(df_clean, columns_to_keep)
 
+avg_median_income <- combined_df %>%
+  summarize(median_income_avg = mean(Median, na.rm = TRUE)) %>%
+  pull(median_income_avg)
+
+
+combined_df['difference_from_median'] = avg_median_income - combined_df['Median']
+
 summary_columns <- c("County", "City", "Zip_Code", "Mean", "Median")
 summary_df <- select(combined_df, summary_columns)
+
 
