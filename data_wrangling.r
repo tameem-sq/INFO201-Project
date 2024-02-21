18a16a8952b747da2405e3b46aa9ae8c8b7ef35f
sophiasmolinski <ssmolin@uw.edu>
2024-02-20 16:35:34 -0800

    Merge college readiness dataset and income data set

--- data_wrangling.r
+++ data_wrangling.r
@@ -1,3 +1,14 @@
-"Hello"
+library("dplyr")
 
-1 + 1
+income_df <- read.csv("kaggle_income.csv")
+college_df <- read.csv("mich_college_readiness.csv")
+
+
+income_mich_df <- income_df %>%
+  filter(State_Name == "Michigan")
+
+combined_income_college <- left_join(income_mich_df, college_df, by=c("Zip_Code" = "ZCTA"))
+
+df_clean <- na.omit(combined_income_college)
+
+
