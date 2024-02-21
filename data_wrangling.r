874f6c03517f8c3dee7c0c3b59e211b933d5cfde
sophiasmolinski <ssmolin@uw.edu>
2024-02-20 17:08:52 -0800

    Working on the summary df

--- data_wrangling.r
+++ data_wrangling.r
@@ -17,3 +17,7 @@ df_clean <- select(-id)
 columns_to_keep <- c("County", "City", "Zip_Code", "Area_Code", "Mean", "Median", "Stdev", "sum_w", "MathNumAssessed", "MathNumReady", "AllSbjtNumReady", "AllSbjtNumAssessed", "EBRWNumAssessed", "EBRWNumReady", "FinalMathAveScore", "FinalAllSbjtAveScore", "FinalEWBRWAveScore", "MathPctReady", "AllSbjtPctReady", "EBRWPctReady")
 
 combined_df <- select(df_clean, columns_to_keep)
+
+summary_columns <- c("County", "City", "Zip_Code", "Mean", "Median")
+summary_df <- select(combined_df, summary_columns)
+
