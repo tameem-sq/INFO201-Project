c6db83e5c61f37238ab7500d922eab45604cb7aa
Helen Xie <hxie8@uw.edu>
2024-03-03 17:10:58 -0800

    add graph 1

--- shiny app/server.R
+++ shiny app/server.R
@@ -2,5 +2,15 @@
 server <- function(input, output){
   
   # TODO Make outputs based on the UI inputs here
+  output$Median_vs_SbjtScore <- renderPlotly({
+    Median_vs_SbjtScore <- ggplot(unified_df) +
+      geom_point(
+        mapping = aes(
+          x = Median,
+          y = FinalAllSbjtAveScore)
+      ) +
+      labs(title = input$viz_1_title)
+    return(ggplotly(Median_vs_SbjtScore))
+  })
   
 }
